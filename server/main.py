3db789cc213c428897abafaaef84b8620c092cfb
Deepak Mardi <deepakmardi22@gmail.com>
2025-08-25 20:57:56 +0530

    Now read from the ALLOWED_ORIGINS environment variable

--- server/main.py
+++ server/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=os.getenv("ALLOWED_ORIGINS", "").split(","),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
